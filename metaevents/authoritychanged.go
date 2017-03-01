4495fea5b1ff1f603e59133f9e61151d7b56bb52
joonas.fi <pompo500@xs.fi>
2017-03-01 18:29:55 +0200

    Add timestamp to AuthorityChanged

--- metaevents/authoritychanged.go
+++ metaevents/authoritychanged.go
@@ -1,14 +1,20 @@
 package metaevents
 
-// .{"_":"AuthorityChange","peers":["127.0.0.1"]}
+import (
+	"time"
+)
+
+// .{"_":"AuthorityChanged","peers":["127.0.0.1"],"ts":"2017-02-27T17:12:31.446Z"}
 type AuthorityChanged struct {
-	Type  string   `json:"_"`
-	Peers []string `json:"peers"`
+	Type      string   `json:"_"`
+	Peers     []string `json:"peers"`
+	Timestamp string   `json:"ts"`
 }
 
 func NewAuthorityChanged(peers []string) *AuthorityChanged {
 	return &AuthorityChanged{
-		Type:  "AuthorityChanged",
-		Peers: peers,
+		Type:      "AuthorityChanged",
+		Peers:     peers,
+		Timestamp: time.Now().Format("2006-01-02T15:04:05.999Z"),
 	}
 }
