b50ac2e4bd3ae39a6f3adcfd63b9da03943c3403
joonas.fi <pompo500@xs.fi>
2017-03-03 10:56:59 +0200

    Support context deadline

--- scalablestore/s3manager.go
+++ scalablestore/s3manager.go
@@ -1,11 +1,13 @@
 package scalablestore
 
 import (
+	"context"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/function61/eventhorizon/config"
 	"io"
+	"time"
 )
 
 type ScalableStoreGetResponse struct {
@@ -40,13 +42,23 @@ func (s *S3Manager) Put(key string, body io.ReadSeeker) error {
 	return err
 }
 
+// this can panic() on context deadline
 func (s *S3Manager) Get(key string) (*ScalableStoreGetResponse, error) {
-	response, err := s.s3Client.GetObject(&s3.GetObjectInput{
+	request, response := s.s3Client.GetObjectRequest(&s3.GetObjectInput{
 		Bucket: &s.bucketName,
 		Key:    &key,
 	})
 
-	if err != nil { // FIXME: assuming 404, not any other error like network error..
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	// cancel is advised to be used always, even on successes
+	// TODO: doesn't seem to work here, probably Send() returns before the whole request is read, because
+	//       we got unexpected EOF with cancel(). that's good because it means S3 client supports streaming
+	// defer cancel()
+
+	// monkey patch the request to use our context
+	request.HTTPRequest = request.HTTPRequest.WithContext(ctx)
+
+	if err := request.Send(); err != nil { // FIXME: assuming 404, not any other error like network error..
 		return nil, err
 	}
 
