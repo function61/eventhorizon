ce18ea6188c8bfe0aea80712cb60f66da8d71881
joonas.fi <pompo500@xs.fi>
2017-03-13 10:18:05 +0200

    Pusher: hook into pub/sub notifications for the target's subscription

--- pusher/pusher.go
+++ pusher/pusher.go
@@ -2,7 +2,10 @@ package pusher
 
 import (
 	"errors"
+	"fmt"
+	"github.com/function61/pyramid/config"
 	"github.com/function61/pyramid/cursor"
+	"github.com/function61/pyramid/pubsub/client"
 	ptypes "github.com/function61/pyramid/pusher/types"
 	"github.com/function61/pyramid/reader"
 	rtypes "github.com/function61/pyramid/reader/types"
@@ -43,19 +46,21 @@ type WorkInput struct {
 }
 
 type Pusher struct {
-	receiver ptypes.Receiver
-	reader   *reader.EventstoreReader
-	stopping bool
-	done     *sync.WaitGroup
-	streams  map[string]*StreamStatus
+	receiver     ptypes.Receiver
+	reader       *reader.EventstoreReader
+	pubSubClient *client.PubSubClient
+	stopping     bool
+	done         *sync.WaitGroup
+	streams      map[string]*StreamStatus
 }
 
 func New(receiver ptypes.Receiver) *Pusher {
 	return &Pusher{
-		receiver: receiver,
-		reader:   reader.New(),
-		done:     &sync.WaitGroup{},
-		streams:  make(map[string]*StreamStatus),
+		receiver:     receiver,
+		pubSubClient: client.New(fmt.Sprintf("%s:%d", "127.0.0.1", config.PUBSUB_PORT)),
+		reader:       reader.New(),
+		done:         &sync.WaitGroup{},
+		streams:      make(map[string]*StreamStatus),
 	}
 }
 
@@ -181,6 +186,8 @@ func (p *Pusher) Run() {
 
 	subscriptionStreamPath := "/_subscriptions/" + subscriptionId
 
+	p.pubSubClient.Subscribe("sub:" + subscriptionId)
+
 	p.streams[subscriptionStreamPath] = &StreamStatus{
 		Stream:    subscriptionStreamPath,
 		shouldRun: true,
@@ -214,62 +221,109 @@ func (p *Pusher) Run() {
 		if inFlight == 0 {
 			if p.stopping {
 				log.Printf("Pusher: runner stopping")
+				return
 			} else {
-				log.Printf("Pusher: unexpectedly nothing to do - stopping")
+				log.Printf("Pusher: nothing to do")
 			}
-			return
 		}
 
-		output := <-responseCh
+		select {
+		case output := <-responseCh:
+			inFlight--
+			p.done.Done()
 
-		inFlight--
-		p.done.Done()
+			inputWas := output.OldInput
 
-		inputWas := output.OldInput
+			concerningStream := inputWas.Status.Stream
 
-		concerningStream := inputWas.Status.Stream
+			p.streams[concerningStream].isRunning = false
+			p.streams[concerningStream].shouldRun = output.ShouldContinueRunning
 
-		p.streams[concerningStream].isRunning = false
-		p.streams[concerningStream].shouldRun = output.ShouldContinueRunning
+			for _, inte := range output.ActivityIntelligence {
+				p.processIntelligence(inte)
+			}
+		case notificationMsg := <-p.pubSubClient.Notifications:
+			log.Printf("Pusher: notification from pubsub: %v", notificationMsg)
 
-		p.processIntelligence(output)
+			if notificationMsg[0] != "NOTIFY" {
+				break
+			}
+
+			cur := cursor.CursorFromserializedMust(notificationMsg[2])
+
+			inte := &StreamStatus{
+				Stream:              cur.Stream,
+				writerLargestCursor: cur,
+				shouldRun:           true,
+			}
+
+			p.processIntelligence(inte)
+		}
 	}
 }
 
-func (p *Pusher) processIntelligence(output *WorkOutput) {
-	for _, inte := range output.ActivityIntelligence {
-		if _, exists := p.streams[inte.Stream]; !exists {
-			p.streams[inte.Stream] = &StreamStatus{
-				Stream:    inte.Stream,
-				shouldRun: true,
-			}
+func (p *Pusher) processIntelligence(inte *StreamStatus) {
+	if _, exists := p.streams[inte.Stream]; !exists {
+		p.streams[inte.Stream] = &StreamStatus{
+			Stream:    inte.Stream,
+			shouldRun: true,
 		}
+	}
 
-		// have intelligence on target status?
-		if inte.targetAckedCursor != nil {
-			// we didn't have previous information => copy as is
-			if p.streams[inte.Stream].targetAckedCursor == nil {
-				p.streams[inte.Stream].targetAckedCursor = inte.targetAckedCursor
+	stored := p.streams[inte.Stream]
+
+	if inte.writerLargestCursor != nil {
+		// we didn't have previous information => copy as is
+		if stored.writerLargestCursor == nil {
+			stored.writerLargestCursor = inte.writerLargestCursor
+
+			log.Printf(
+				"Pusher: %s Writer known largest initialized @ %s",
+				inte.writerLargestCursor.Stream,
+				inte.writerLargestCursor.OffsetString())
+		} else {
+			// have information => compare if provided information is ahead
+			if inte.writerLargestCursor.IsAheadComparedTo(stored.writerLargestCursor) {
+				stored.writerLargestCursor = inte.writerLargestCursor
+				stored.shouldRun = true
 
 				log.Printf(
-					"Pusher: %s initialized @ %s",
+					"Pusher: %s Writer known largest forward @ %s",
+					inte.writerLargestCursor.Stream,
+					inte.writerLargestCursor.OffsetString())
+			} else {
+				log.Printf(
+					"Pusher: %s Writer known largest outdated @ %s",
+					inte.writerLargestCursor.Stream,
+					inte.writerLargestCursor.OffsetString())
+			}
+		}
+	}
+
+	// have intelligence on target status?
+	if inte.targetAckedCursor != nil {
+		// we didn't have previous information => copy as is
+		if stored.targetAckedCursor == nil {
+			stored.targetAckedCursor = inte.targetAckedCursor
+
+			log.Printf(
+				"Pusher: %s Target initialized @ %s",
+				inte.targetAckedCursor.Stream,
+				inte.targetAckedCursor.OffsetString())
+		} else {
+			// have information => compare if provided information is ahead
+			if inte.targetAckedCursor.IsAheadComparedTo(stored.targetAckedCursor) {
+				stored.targetAckedCursor = inte.targetAckedCursor
+
+				log.Printf(
+					"Pusher: %s Target forward @ %s",
 					inte.targetAckedCursor.Stream,
 					inte.targetAckedCursor.OffsetString())
 			} else {
-				// have information => compare if provided information is ahead
-				if inte.targetAckedCursor.IsAheadComparedTo(p.streams[inte.Stream].targetAckedCursor) {
-					p.streams[inte.Stream].targetAckedCursor = inte.targetAckedCursor
-
-					log.Printf(
-						"Pusher: %s forward @ %s",
-						inte.targetAckedCursor.Stream,
-						inte.targetAckedCursor.OffsetString())
-				} else {
-					log.Printf(
-						"Pusher: %s backpedal/stay still @ %s",
-						inte.targetAckedCursor.Stream,
-						inte.targetAckedCursor.OffsetString())
-				}
+				log.Printf(
+					"Pusher: %s Target backpedal/stay still @ %s",
+					inte.targetAckedCursor.Stream,
+					inte.targetAckedCursor.OffsetString())
 			}
 		}
 	}
@@ -281,6 +335,8 @@ func (p *Pusher) Close() {
 	log.Printf("Pusher: stopping")
 
 	p.done.Wait()
+
+	p.pubSubClient.Close()
 
 	log.Printf("Pusher: stopped")
 }
