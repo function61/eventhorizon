4e30c73c4fc668c9b429e47cc1d56f4af5982fb7
joonas.fi <pompo500@xs.fi>
2017-03-14 14:01:04 +0200

    pusher-receiver-failing-proxy: make fail 75 % of the time

--- cli/pyramid/pusher-receiver-failing-proxy.go
+++ cli/pyramid/pusher-receiver-failing-proxy.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	ptypes "github.com/function61/pyramid/pusher/types"
 	rtypes "github.com/function61/pyramid/reader/types"
 )
 
 type FailingReceiverProxy struct {
-	next ptypes.Receiver
+	next    ptypes.Receiver
+	counter int
 }
 
 func NewFailingReceiverProxy(next ptypes.Receiver) *FailingReceiverProxy {
 	return &FailingReceiverProxy{
-		next: next,
+		next:    next,
+		counter: 1, // so first req fails
 	}
 }
 
 func (f *FailingReceiverProxy) PushReadResult(result *rtypes.ReadResult) (*ptypes.PushResult, error) {
+	if f.shouldFail() {
+		return nil, errors.New(fmt.Sprintf("synthetic failure %d", f.counter))
+	}
+
 	return f.next.PushReadResult(result)
 }
 
 func (f *FailingReceiverProxy) GetSubscriptionId() (string, error) {
 	return f.next.GetSubscriptionId()
+	/*
+		if f.shouldFail() {
+			return "", errors.New("synthetic failure")
+		}
+	*/
+}
+
+func (f *FailingReceiverProxy) shouldFail() bool {
+	defer func() { f.counter++ }()
+
+	if f.counter%4 == 0 {
+		return false // make every 4th request succeed
+	}
+
+	return true
 }
