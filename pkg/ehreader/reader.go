4763d829bdc10a3ca8f15c4a9fd7adc9834590f3
Joonas Loppi <joonas@joonas.fi>
2020-02-27 15:54:50 +0200

    ehreader: add TransactWrite() helper

--- pkg/ehreader/reader.go
+++ pkg/ehreader/reader.go
@@ -238,6 +238,34 @@ func (r *Reader) LoadUntilRealtime(ctx context.Context) error {
 	}
 }
 
+// wraps your AppendAfter() result with state-refreshed retries for ErrOptimisticLockingFailed
+// FIXME: currently this cannot be used along with Synchronizer(), because the Reader
+//        is not safe for concurrent use
+func (r *Reader) TransactWrite(ctx context.Context, fn func() error) error {
+	maxTries := 4
+	var err error
+
+	for i := 0; i < maxTries; i++ {
+		err = fn()
+		if err == nil {
+			return nil // success
+		}
+
+		if _, wasAboutLocking := err.(*ehclient.ErrOptimisticLockingFailed); !wasAboutLocking {
+			return err // some other error
+		}
+
+		r.logl.Debug.Printf("ErrOptimisticLockingFailed, try %d: %v", i+1, err)
+
+		// reach realtime again, so we can try again
+		if err := r.LoadUntilRealtime(ctx); err != nil {
+			return err
+		}
+	}
+
+	return fmt.Errorf("maxTries failed (%d): %v", maxTries, err)
+}
+
 // helper for your code to generate an error
 func UnsupportedEventTypeErr(e ehevent.Event) error {
 	return fmt.Errorf("unsupported event type: %s", e.MetaType())
--- pkg/ehreader/reader_test.go
+++ pkg/ehreader/reader_test.go
@@ -1,6 +1,7 @@
 package ehreader
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"github.com/function61/eventhorizon/pkg/ehevent"
 	"github.com/function61/eventhorizon/pkg/ehreader/ehreadertest"
 	"github.com/function61/gokit/assert"
+	"log"
 	"strings"
 	"testing"
 	"time"
@@ -41,6 +43,71 @@ func TestReaderReadIntoProjection(t *testing.T) {
 13:45:00 joonas: Testing first message
 13:47:00 joonas: Is anybody listening?
 14:32:00 joonas: So lonely :(`)
+}
+
+func TestTransactWriteFailsEachTry(t *testing.T) {
+	stream := "/chatrooms/offtopic"
+
+	eventLog := ehreadertest.NewEventLog()
+	eventLog.AppendE(stream, NewChatMessage(1, "Testing first message", ehevent.Meta(t0, "joonas")))
+
+	reader := New(newChatRoomProjection(stream), eventLog, nil)
+
+	tryNumber := 0
+
+	err := reader.TransactWrite(context.Background(), func() error {
+		tryNumber++
+
+		return ehclient.NewErrOptimisticLockingFailed(fmt.Errorf("try %d", tryNumber))
+	})
+
+	assert.EqualString(t, err.Error(), "maxTries failed (4): try 4")
+}
+
+func TestTransactWriteSucceedsOnThirdTry(t *testing.T) {
+	ctx := context.Background()
+
+	stream := "/chatrooms/offtopic"
+
+	eventLog := ehreadertest.NewEventLog()
+	eventLog.AppendE(stream, NewChatMessage(1, "Testing first message", ehevent.Meta(t0, "joonas")))
+
+	chatRoom := newChatRoomProjection(stream)
+	chatRoom.includeSequenceNumbers = true
+
+	logBuf := &bytes.Buffer{}
+
+	reader := New(chatRoom, eventLog, log.New(logBuf, "", 0))
+
+	assert.Ok(t, reader.LoadUntilRealtime(ctx))
+
+	tryNumber := 0
+
+	assert.Ok(t, reader.TransactWrite(ctx, func() error {
+		tryNumber++
+
+		// inject new chat message so our Append() won't succeed on this try
+		if tryNumber == 1 || tryNumber == 2 {
+			eventLog.AppendE(stream, NewChatMessage(len(chatRoom.chatLog)+1, "Conflict causing message", ehevent.Meta(t0, "memelord")))
+		}
+
+		msg := NewChatMessage(len(chatRoom.chatLog)+1, "My last message", ehevent.Meta(t0, "joonas"))
+
+		_, err := eventLog.AppendAfter(ctx, chatRoom.cur, []string{ehevent.Serialize(msg)})
+		return err
+	}))
+
+	assert.EqualString(t, logBuf.String(), `[DEBUG] ErrOptimisticLockingFailed, try 1: conflict: afterRequested=/chatrooms/offtopic@1 afterActual=/chatrooms/offtopic@2
+[DEBUG] ErrOptimisticLockingFailed, try 2: conflict: afterRequested=/chatrooms/offtopic@2 afterActual=/chatrooms/offtopic@3
+`)
+
+	assert.Ok(t, reader.LoadUntilRealtime(ctx))
+
+	assert.EqualString(t, chatRoom.PrintChatLog(), `
+13:45:00 1 joonas: Testing first message
+13:45:00 2 memelord: Conflict causing message
+13:45:00 3 memelord: Conflict causing message
+13:45:00 4 joonas: My last message`)
 }
 
 func newChatRoomProjection(stream string) *chatRoomProjection {
