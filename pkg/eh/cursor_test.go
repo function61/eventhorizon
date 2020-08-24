d9975ac0234c6dee3d62ebee9698ee1559881101
Joonas Loppi <joonas@joonas.fi>
2020-08-24 14:39:33 +0300

    eh: extract streamname into own file

--- pkg/eh/cursor_test.go
+++ pkg/eh/cursor_test.go
@@ -68,29 +68,3 @@ func TestBeginning(t *testing.T) {
 	assert.EqualString(t, beg.Serialize(), "/foo@-1")
 	assert.Assert(t, beg.Equal(foo.At(-1)))
 }
-
-func TestStreamName(t *testing.T) {
-	assert.Assert(t, RootName.Parent() == nil)
-
-	assert.EqualString(t, RootName.ResourceName().String(), "f61rn:eventhorizon:stream:/")
-
-	uid3 := RootName.Child("t-3").Child("users").Child("uid3")
-
-	assert.EqualString(t, uid3.String(), "/t-3/users/uid3")
-	assert.EqualString(t, uid3.Parent().String(), "/t-3/users")
-	assert.EqualString(t, uid3.Parent().Parent().String(), "/t-3")
-	assert.EqualString(t, uid3.Parent().Parent().Parent().String(), "/")
-	assert.Assert(t, uid3.Parent().Parent().Parent().Parent() == nil)
-}
-
-func TestStreamNameIsUnder(t *testing.T) {
-	uid3 := RootName.Child("t-3").Child("users").Child("uid3")
-
-	// everything's under root name
-	assert.Assert(t, uid3.IsUnder(RootName))
-	assert.Assert(t, uid3.IsUnder(RootName.Child("t-3")))
-	assert.Assert(t, uid3.IsUnder(RootName.Child("t-3").Child("users")))
-	assert.Assert(t, !uid3.IsUnder(RootName.Child("t-3").Child("usErs")))
-	assert.Assert(t, !uid3.IsUnder(RootName.Child("t-4")))
-	assert.Assert(t, !uid3.IsUnder(RootName.Child("t-4").Child("users")))
-}
---
+++ pkg/eh/streamname.go
@@ -0,0 +1,74 @@
+package eh
+
+import (
+	"errors"
+	"fmt"
+	"path"
+	"strings"
+
+	"github.com/function61/eventhorizon/pkg/policy"
+)
+
+type StreamName struct {
+	name string
+}
+
+func newStreamNameNoValidation(name string) StreamName {
+	return StreamName{name}
+}
+
+// TODO: add better validation
+func DeserializeStreamName(name string) (StreamName, error) {
+	if name == "" {
+		return StreamName{}, errors.New("StreamName empty")
+	}
+
+	return newStreamNameNoValidation(name), nil
+}
+
+var RootName = newStreamNameNoValidation("/")
+
+func (s StreamName) Child(name string) StreamName {
+	if name == "" {
+		panic(fmt.Errorf("tried to create empty StreamName child for '%s'", s.name))
+	}
+
+	if strings.Contains(name, "/") {
+		panic("StreamName child cannot contain '/'")
+	}
+
+	return StreamName{path.Join(s.name, name)}
+}
+
+func (s StreamName) String() string {
+	return s.name
+}
+
+func (s StreamName) IsUnder(other StreamName) bool {
+	return strings.HasPrefix(s.name, other.name)
+}
+
+func (s StreamName) ResourceName() policy.ResourceName {
+	return ResourceNameStream.Child(s.String())
+}
+
+// "/foo" => "/"
+// "/foo/bar" => "/foo"
+// "/" => nil
+func (s StreamName) Parent() *StreamName {
+	parent := path.Dir(s.name)
+	if parent == s.name { // already at root? Dir("/") == "/"
+		return nil
+	}
+
+	return &StreamName{parent}
+}
+
+func (s StreamName) At(version int64) Cursor {
+	return Cursor{s.String(), version}
+}
+
+// in context of "give events > -1" (not >=). the actual beginning is 0
+func (s StreamName) Beginning() Cursor {
+	return s.At(-1)
+}
